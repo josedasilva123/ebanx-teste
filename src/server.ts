1f0d2d90b80e59574370abac9f6fc39c22ed31f0
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-06-05 18:47:56 -0300

    starting setup

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import { app } from "./app";
+
+const port = process.env.PORT || 3001;
+
+app.listen({ host: "0.0.0.0", port: Number(port) }, () => {
+    console.log(`API successfully started at port ${port}`);
+});
