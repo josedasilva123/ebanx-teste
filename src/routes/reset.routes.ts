0fdf3d5ede15bc02900d552b0c3d60dab26a5365
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-06-05 20:15:02 -0300

    chore: adding empty body to prevent fastify error

--- src/routes/reset.routes.ts
+++ src/routes/reset.routes.ts
@@ -2,5 +2,9 @@ import { FastifyInstance } from "fastify";
 import { reset } from "../controllers/reset.controller";
 
 export const resetRoutes = async (fastify: FastifyInstance) => {
-    fastify.post("/", reset);
+    fastify.post("/", { preHandler: (req, res, done) => {
+        req.body = {};
+
+        done();
+    }}, reset);
 }
