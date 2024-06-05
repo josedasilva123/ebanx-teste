f050f6eae1f2f9101766b3c7cf10e94cdfdda3bd
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-06-05 19:55:21 -0300

    chore: splice instead of slice

--- src/controllers/reset.controller.ts
+++ src/controllers/reset.controller.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { accountDatabase } from "../database/account";
 
 export const reset = (request: FastifyRequest, response: FastifyReply) => {
-    accountDatabase.slice(0, accountDatabase.length);
+    accountDatabase.splice(0, accountDatabase.length);
 
     return response.status(200).send({});
 }
